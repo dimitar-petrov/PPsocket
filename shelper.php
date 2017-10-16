78cdae91a87aec145abde936529e926625a06169
Dimitar Petrov <petrov.dimp@gmail.com>
2017-10-16 07:19:04 +0300

    Updates to both

--- shelper.php
+++ shelper.php
@@ -4,20 +4,14 @@ class SocketHelper{
    
    public $socket;
 
-   public function __construct($host,$port){
-       $this->socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-   	   $result = socket_connect($this->socket,$host,$port);
+   public function __construct($unix_socket){
+       $this->socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
+   	   $result = socket_connect($this->socket, $unix_socket);
    }
 
    public function send_data($content){
        socket_write($this->socket, 100000 + strlen($content), 6);
-       socket_write($this->socket,$content,strlen($content));
-   }
-
-   public function read_data(){
-   	   while($out = socket_read($this->socket,3)){
-	       return $out;
-       }
+       socket_write($this->socket, $content, strlen($content));
    }
 
    public function close_socket(){
--- shelper.py
+++ shelper.py
@@ -1,27 +1,46 @@
 import socket
+import select
+import os
 
-class SocketHelper:
+class SockServer:
 
     s = None
-    conn = None
-    addr = None
+    timeout = 0.1
 
-    def __init__(self,host,port):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((host, port))
-        self.s.listen(5)
+    def __init__(self, unix_socket):
+        # Make sure the socket does not already exist
 
-    def s_appept(self):
-       self.conn,self.addr = self.s.accept()
+        try:
+            os.unlink(unix_socket)
+        except OSError:
+            if os.path.exists(unix_socket):
+                raise
 
-    def send_data(self,content):
-       self.conn.send(content)
+        self.s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+        self.s.bind(unix_socket)
 
     def read_data(self):
-        count = int(self.conn.recv(6))
-        buf = self.conn.recv(count-100000)
-        return buf
+
+        ready = select.select([self.s], [], [], self.timeout)
+        if ready[0]:
+            size = int(self.s.recv(6))
+            buf = self.s.recv(size - 100000)
+            return buf
 
     def close_socket(self):
-		self.conn.close()
+        self.s.close()
 
+class SockClient:
+    s = None
+
+    def __init__(self, unix_socket):
+        self.s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+        self.s.connect(unix_socket)
+
+    def write_data(self, data):
+        size = 100000 + len(data)
+        self.s.send(str(size).encode('utf-8'))
+        self.s.send(data.encode('utf-8'))
+
+    def close_socket(self):
+        self.s.close()
